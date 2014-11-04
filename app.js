8c616a85f223a0e0edc89ba14d6c73680ffb362c
Joel Skrepnek <jskrepnek@gmail.com>
2014-11-04 06:29:55 -0800

    Solution

--- app.js
+++ app.js
@@ -9,3 +9,42 @@ app.config(function($routeProvider) {
 			redirectTo: '/'
 		});
 });
+
+app.controller('HomeController', ['$scope', 
+	function($scope){
+		$scope.log = function() {
+			console.log($scope.form);
+		}
+}])
+
+app.directive('aaPattern', [function(){
+	return {
+		require: 'ngModel',
+		link: function($scope, iElm, iAttrs, controller) {
+			var PATTERN = /A.*A/;
+			controller.$validators.aaPattern = function(modelValue, viewValue) {
+				return PATTERN.test(viewValue);
+			};
+		}
+	};
+}]);
+
+app.directive('unique', ['$q', '$timeout', function($q, $timeout) {
+	return {
+		require: 'ngModel',
+		link: function($scope, element, attributes, controller) {
+			var existingUsernames = ['galaxy', 'stars', 'planets', 'pluto'];			
+			controller.$asyncValidators.unique = function(modelValue, viewValue) {
+				var defer = $q.defer();
+				$timeout(function() {
+					if (existingUsernames.indexOf(modelValue) === -1) {
+						defer.resolve();
+					} else {
+						defer.reject();
+					}
+				}, 2000);
+				return defer.promise;
+			};
+		}
+	};
+}])
